b3cfe57fdf31030a42ff1a5ce90fed8ef11f6c53
mippet <pimluitjes@gmail.com>
2017-01-29 17:39:52 +0100

    failed in updating, skipped it for now

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,9 +20,26 @@ class ProductsController < ApplicationController
     redirect_to @product.box
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to @product.box
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :image)
+    params.require(:product).permit(:title, :description, :price, :image, :box_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 root to: "boxes#index"
 
 resources :boxes, only: [:create, :destroy, :show] do
-  resources :products, only: [:create, :destroy]
+  resources :products, only: [:create, :destroy, :edit, :update, :show]
 end
 
 end
