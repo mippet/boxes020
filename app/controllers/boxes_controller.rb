01429c70ccc485eb2781972e6f5f3d02f42dd1b4
mippet <pimluitjes@gmail.com>
2017-01-29 14:11:22 +0100

    possible to add new boxes

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,6 +1,22 @@
 class BoxesController < ApplicationController
 
   def index
+    @boxes = Box.all
+  end
 
+  def new
+   @box = Box.new
+  end
+
+  def create
+  @box = Box.new(box_params)
+  @box.save
+
+  end
+
+  private
+
+  def box_params
+    params.require(:box).permit(:title, :description, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 root to: "boxes#index"
+
+resources :boxes, only: [:create]
 end
